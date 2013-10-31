3626ab4fba0e2acd5c70f435fb6f4950fcaf2db6
Alex Angelini <alex.louis.angelini@gmail.com>
2013-10-31 18:04:43 -0400

    Fixed spacing

--- src/main/java/com/mcgill/bandop/Bandop.java
+++ src/main/java/com/mcgill/bandop/Bandop.java
@@ -16,31 +16,31 @@ public class Bandop {
 	static final String DB_USER = "bandop";
 	static final String DB_PASS = "bandop";
 
-    public static void main( String[] args ) {
-    	Server server = new Server(8080);
+	public static void main( String[] args ) {
+		Server server = new Server(8080);
 
-    	ServletContextHandler context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
+		context.setContextPath("/");
+		server.setHandler(context);
 
-        ServletHolder jerseyServlet = context.addServlet(ServletContainer.class, "/api/*");
-        jerseyServlet.setInitOrder(1);
-        jerseyServlet.setInitParameter("javax.ws.rs.Application", "com.mcgill.bandop.Application");
+		ServletHolder jerseyServlet = context.addServlet(ServletContainer.class, "/api/*");
+		jerseyServlet.setInitOrder(1);
+		jerseyServlet.setInitParameter("javax.ws.rs.Application", "com.mcgill.bandop.Application");
 
-        ServletHolder staticServlet = context.addServlet(DefaultServlet.class, "/*");
-        staticServlet.setInitParameter("resourceBase", "src/main/app");
-        staticServlet.setInitParameter("pathInfoOnly", "true");
+		ServletHolder staticServlet = context.addServlet(DefaultServlet.class, "/*");
+		staticServlet.setInitParameter("resourceBase", "src/main/app");
+		staticServlet.setInitParameter("pathInfoOnly", "true");
 
-        try {
-        	Class.forName("org.postgresql.Driver");
-        	Connection conn = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASS);
-        	context.setAttribute("db", new Database(conn));
+		try {
+			Class.forName("org.postgresql.Driver");
+			Connection conn = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASS);
+			context.setAttribute("db", new Database(conn));
 
-            server.start();
-            server.join();
-        } catch (Throwable t) {
-            t.printStackTrace(System.err);
-        }
-    }
+			server.start();
+			server.join();
+		} catch (Throwable t) {
+			t.printStackTrace(System.err);
+		}
+	}
 
 }
